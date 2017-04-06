8825b31dd840b6b2eaa01a8f3cb22e97fab686f9
kitkatdesu <kitkatdesu@users.noreply.github.com>
2017-04-06 20:46:29 +1200

    add source code command

---
+++ commands/info/source.ts
@@ -0,0 +1,16 @@
+import commando = require('discord.js-commando')
+
+module.exports = class SourceCommand extends commando.Command {
+    constructor(client: commando.CommandoClient) {
+        super(client, {
+            name: 'source',
+            group: 'info',
+            memberName: 'source',
+            description: 'Gets a source code link for Azure.'
+        })
+    }
+
+    async run(message, args) {
+        return message.reply("View the source code here: https://github.com/kitkatdesu/azure")
+    }
+}

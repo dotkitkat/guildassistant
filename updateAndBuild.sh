c423dc7a0155cc243c156bfe1e95659e6ca01ac3
kitkatdesu <kitkatdesu@users.noreply.github.com>
2017-04-06 15:59:13 +1200

    updates

--- updateAndBuild.sh
+++ updateAndBuild.sh
@@ -1,7 +1,4 @@
 echo "Building Azure..."
-echo "Killing existing Azure processes... (This will kill all Node.js processes)"
-killall nodejs
-echo "Done."
 echo "Updating Azure..."
 sudo git pull
 echo "Done."

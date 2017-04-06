687f4663f5576393fd1a0cbb24e320c011d9d5a9
kitkatdesu <kitkatdesu@users.noreply.github.com>
2017-04-06 15:47:37 +1200

    build script updates

--- updateAndBuild.sh
+++ updateAndBuild.sh
@@ -1,4 +1,15 @@
-echo "Updating and Building Azure..."
+echo "Building Azure..."
+echo "Killing existing Azure processes... (This will kill all Node.js processes)"
+killall nodejs
+echo "Done."
+echo "Updating Azure..."
 sudo git pull
+echo "Done."
+echo "Compiling.."
 sudo tsc -p .
+echo "Done."
+echo "Moving to build folder.."
 cd build
+echo "Done."
+echo "Running Azure.."
+node --harmony index.js
